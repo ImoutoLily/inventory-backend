3ac9d599bbdb1e173f608d99a6c069ee90a99d9b
Shirley Fox <keanu.vandenbore007@hotmail.com>
2022-05-10 12:52:41 +0200

    Create gateway implementations

---
+++ Inventory.Gateways/GetInventoriesGateway.cs
@@ -0,0 +1,23 @@
+﻿using Inventory.Core.Business.Gateways;
+using Inventory.Database.Context;
+using Microsoft.EntityFrameworkCore;
+
+namespace Inventory.Gateways;
+
+public class GetInventoriesGateway : IGetInventoriesGateway
+{
+    private readonly InventoryContext _context;
+
+    public GetInventoriesGateway(InventoryContext context)
+    {
+        _context = context;
+    }
+
+    public async Task<IList<Core.Models.Inventory>> GetAll()
+    {
+        var inventories = await _context.Inventories
+            .ToListAsync();
+
+        return inventories;
+    }
+}
---
+++ Inventory.Gateways/SaveInventoryGateway.cs
@@ -0,0 +1,26 @@
+﻿using Inventory.Core.Business.Gateways;
+using Inventory.Database.Context;
+
+namespace Inventory.Gateways;
+
+public class SaveInventoryGateway : ISaveInventoryGateway
+{
+    private readonly InventoryContext _context;
+
+    public SaveInventoryGateway(InventoryContext context)
+    {
+        _context = context;
+    }
+
+    public async Task<Core.Models.Inventory> Save(string name)
+    {
+        var inventory = new Core.Models.Inventory
+        {
+            Name = name
+        };
+
+        await _context.Inventories.AddAsync(inventory);
+
+        return inventory;
+    }
+}
