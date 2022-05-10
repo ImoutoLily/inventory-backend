3ace167fa639ba29689bbd8ae95652e8fa40702b
Shirley Fox <keanu.vandenbore007@hotmail.com>
2022-05-10 11:22:20 +0200

    Create Inventory and InventoryItem models

---
+++ Inventory.Core.Models/Inventory.cs
@@ -0,0 +1,7 @@
+﻿namespace Inventory.Core.Models;
+
+public class Inventory
+{
+    public string Name { get; set; } = null!;
+    public IList<InventoryItem> Items { get; set; } = new List<InventoryItem>();
+}
---
+++ Inventory.Core.Models/InventoryItem.cs
@@ -0,0 +1,9 @@
+﻿namespace Inventory.Core.Models;
+
+public class InventoryItem
+{
+    public string Name { get; set; } = null!;
+    public int Count { get; set; }
+    public double? PricePerItem { get; set; }
+    public string? AdditionalInformation { get; set; }
+}
