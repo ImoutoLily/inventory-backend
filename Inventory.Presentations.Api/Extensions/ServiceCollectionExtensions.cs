dbdb92b541cdfd895e90348612e12974f1fd892b
Shirley Fox <keanu.vandenbore007@hotmail.com>
2022-05-10 14:46:33 +0200

    Refactor Program.cs

---
+++ Inventory.Presentations.Api/Extensions/ServiceCollectionExtensions.cs
@@ -0,0 +1,44 @@
+﻿using Inventory.Core.Business;
+using Inventory.Core.Business.Gateways;
+using Inventory.Database.Context;
+using Inventory.Gateways;
+using Microsoft.EntityFrameworkCore;
+
+namespace Inventory.Presentations.Api.Extensions;
+
+public static class ServiceCollectionExtensions
+{
+    public static IServiceCollection AddInventoryContext(this IServiceCollection services,
+        IWebHostEnvironment environment)
+    {
+        if (environment.IsDevelopment())
+        {
+            services.AddDbContext<InventoryContext>(o 
+                => o.UseInMemoryDatabase(databaseName: "inventory-db"));
+        }
+        else
+        {
+            // TODO: connect to persistent database
+        }
+
+        return services;
+    }
+    
+    public static IServiceCollection AddGateways(this IServiceCollection services)
+    {
+        services.AddTransient<IGetInventoriesGateway, GetInventoriesGateway>()
+            .AddTransient<ISaveInventoryGateway, SaveInventoryGateway>()
+            .AddTransient<IUpdateInventoryGateway, UpdateInventoryGateway>();
+
+        return services;
+    }
+
+    public static IServiceCollection AddUseCases(this IServiceCollection services)
+    {
+        services.AddTransient<CreateInventory>()
+            .AddTransient<GetInventories>()
+            .AddTransient<UpdateInventory>();
+
+        return services;
+    }
+}
