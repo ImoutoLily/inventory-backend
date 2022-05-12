ff8088359a4dd8d0123dbe8ce9689cfea017ec41
Shirley Fox <keanu.vandenbore007@hotmail.com>
2022-05-12 10:59:37 +0200

    Fix username validation

--- Inventory.Core.Business.Validators/UserValidator.cs
+++ Inventory.Core.Business.Validators/UserValidator.cs
@@ -6,7 +6,7 @@ public static class UserValidator
 {
     public static bool IsUsernameValid(string username)
         => Regex.IsMatch(username, 
-            "^(?=.*[a-zA-Z])(?=.*[\\d])(?=.*[\\-_ ])[a-zA-Z\\d\\-_]{4,32}$");
+            "^(?=.*[a-zA-Z])[ a-zA-Z\\d\\-_]{4,32}$");
 
     public static bool IsPasswordValid(string password) 
         => Regex.IsMatch(password, 
