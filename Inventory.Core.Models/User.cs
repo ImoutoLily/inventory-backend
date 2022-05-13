134daf35fa5d9134b00f8f0efcf4d995f74ec307
ImoutoLily <shirley.fox_@hotmail.com>
2022-05-13 10:52:35 +0200

    Add email property to User model

--- Inventory.Core.Models/User.cs
+++ Inventory.Core.Models/User.cs
@@ -3,6 +3,7 @@
 public class User
 {
     public string Id { get; set; } = null!;
+    public string Email { get; set; } = null!;
     public string UserName { get; set; } = null!;
     public IList<Inventory> Inventories { get; set; } = new List<Inventory>();
 }
