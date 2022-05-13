5c50b4e524327c40db146cbecf252e2d2c39b499
ImoutoLily <shirley.fox_@hotmail.com>
2022-05-13 15:01:57 +0200

    Make InventoryItem's Count property nullable so it is not required during a patch

--- Inventory.Adapters.Models/Request/InventoryItemRequest.cs
+++ Inventory.Adapters.Models/Request/InventoryItemRequest.cs
@@ -3,7 +3,7 @@ namespace Inventory.Adapters.Models.Request;
 public class InventoryItemRequest
 {
     public string Name { get; set; } = null!;
-    public int Count { get; set; } = 0; // TODO: nullable
+    public int? Count { get; set; }
     public double? PricePerItem { get; set; }
     public string? AdditionalInformation { get; set; }
 }
